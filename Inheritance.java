9ebfa14f65e002947f0722c003c60d5f03577dad
Your Name <you@example.com>
2018-09-03 22:20:57 +0530

    Inheritance code is added

---
+++ Inheritance.java
@@ -0,0 +1,39 @@
+class Employee
+{
+        int empid;
+        String empName;
+        int age;
+        int salary;
+
+        Employee()
+        {
+           System.out.println("Empty Constructor");OB
+        }
+
+        Employee(int empid,String empName,int age,int salary)
+        {
+                this.empid=empid;
+                this.empName=empName;
+                this.age=age;
+                this.salary=salary;
+        }
+
+        void display()
+        {
+                System.out.println("EmployeeId:"+empid);
+                System.out.println("EmployeeName:"+empName);
+                System.out.println("Age:"+age);
+                System.out.println("Salary:"+salary);
+        }
+}
+
+public Main
+{
+        public static void main(String args[])
+        {
+                Employee employee=new Employee();
+                Employee employee=new Employee(1,"saranya",22,200000);
+                employee.display();
+        }
+}
+
